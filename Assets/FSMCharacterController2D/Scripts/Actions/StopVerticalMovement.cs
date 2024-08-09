f772aa29217a3b308cb7561d7561597ea0c806ba
Abhi86Shek <sonkar.abhishek86@gmail.com>
2024-08-09 20:19:31 +0530

    cut the vertical velocity

---
+++ Assets/FSMCharacterController2D/Scripts/Actions/StopVerticalMovement.cs
@@ -0,0 +1,20 @@
+using SAS.StateMachineGraph;
+using SAS.Utilities.TagSystem;
+
+namespace SAS.StateMachineCharacterController2D
+{
+    public class StopVerticalMovement : IStateAction
+    {
+        [FieldRequiresSelf] private FSMCharacterController2D _characterController;
+        void IStateAction.OnInitialize(Actor actor, Tag tag, string key)
+        {
+            actor.Initialize(this);
+        }
+
+
+        void IStateAction.Execute(ActionExecuteEvent executeEvent)
+        {
+            _characterController.movementVector.y = 0;
+        }
+    }
+}
